02af70675164fb380b44d40c5af7f9b88a4b3513
TheAssembler1 <lewisnoah592@gmail.com>
2022-03-01 20:35:44 -0600

    test

--- Programming-Assignment#2/solution.c
+++ Programming-Assignment#2/solution.c
@@ -217,7 +217,7 @@ void execute_highest_priority_process(void){
 		printf("CURRENT wanted cpu burst time %lu\n", process_behavior->cpuburst);
 
 		//checking if we have ran enought cpu cycles
-		if(++(process_behavior->current_cpuburst) == process_behavior->cpuburst){
+		if(++(process_behavior->current_cpuburst) >= process_behavior->cpuburst){
 			//check if this is the last cpu time we need so we repeated one more time than we need to end on cpu time
 			if(++(process_behavior->current_repeat) > process_behavior->repeat){
 				rewind_queue(&process->behaviors);
