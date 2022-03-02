a1e7f334f66d0e04d5bcfc9d5158bc66e5bd922a
TheAssembler1 <lewisnoah592@gmail.com>
2022-03-01 19:00:47 -0600

    test

--- Programming-Assignment#2/solution.c
+++ Programming-Assignment#2/solution.c
@@ -228,8 +228,7 @@ while(Clock = current_priority(&ArrivalQ)){
 void queue_new_arrivals(void){
 	rewind_queue(&ArrivalQ);
 	while(Clock == current_priority(&ArrivalQ)){
-		void* element = malloc(sizeof(50));
-		remove_from_front(&ArrivalQ, element);
+		delete_current(&ArrivalQ);
 		printf("%d\n", Clock);
 	}
 
