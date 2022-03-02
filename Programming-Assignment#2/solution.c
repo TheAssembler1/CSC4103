32c6d4f0b195372c35f08bba9c9af5da94ec04b7
TheAssembler1 <lewisnoah592@gmail.com>
2022-03-01 21:27:17 -0600

    test

--- Programming-Assignment#2/solution.c
+++ Programming-Assignment#2/solution.c
@@ -121,6 +121,12 @@ int main(int argc, char* argv[]){
 void read_process_descriptions(void){
 	Process p;
 	ProcessBehavior b;
+
+	//FIXME::remove this somehow
+	b.current_cpuburst = 0;
+	b.current_ioburst = 0;
+	b.current_repeat = 0;
+
 	int pid = 0, first = 1;
 	unsigned long arrival;
 
@@ -132,11 +138,6 @@ void read_process_descriptions(void){
 		       &b.cpuburst,
 		       &b.ioburst,
 		       &b.repeat);
-
-		//FIXME::remove this somehow
-		b.current_cpuburst = 0;
-		b.current_ioburst = 0;
-		b.current_repeat = 0;
 		
 		if(!first && p.pid != pid){
 			add_to_queue(&ArrivalQ, &p, p.arrival_time);
