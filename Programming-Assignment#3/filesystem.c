5d1b6df02edfe6a20e4184f236c9386fede7a812
TheAssembler1 <lewisnoah592@gmail.com>
2022-04-15 11:42:37 -0500

    more stuff

--- Programming-Assignment#3/filesystem.c
+++ Programming-Assignment#3/filesystem.c
@@ -200,9 +200,9 @@ int delete_file(char *name){
             struct FileBlock* file_block = (struct FileBlock*)&buffer[file_block_ptr];
 
             //file descriptor with name exists
-            if(!memcmp(file_block->file_name, file->file_block.file_name, strlen(file->file_block.file_name))){
+            if(!memcmp(file_block->file_name, name, strlen(name)){
                 File file = malloc(sizeof(struct FileBlock));
-                memcpy(file->file_block, file_bloc, sizeof(struct FileBlock));
+                memcpy(file->file_block, file_block, sizeof(struct FileBlock));
 
                 uint8_t buffer[SOFTWARE_DISK_BLOCK_SIZE * get_fat_table_size_blocks()];
                 memset(buffer, 0, SOFTWARE_DISK_BLOCK_SIZE * get_fat_table_size_blocks());
